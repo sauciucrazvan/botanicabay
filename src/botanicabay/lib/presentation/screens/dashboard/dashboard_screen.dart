4ac9b8c00847762e7f96f39b39abc22e0c122200
Razvan <razvansau9@gmail.com>
2024-03-23 18:01:31 +0200

    Restylezed the app logo, modified the dashboard app bar

--- src/botanicabay/lib/presentation/screens/dashboard/dashboard_screen.dart
+++ src/botanicabay/lib/presentation/screens/dashboard/dashboard_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:botanicabay/data/providers/test_provider.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class DashboardScreen extends ConsumerWidget {
   const DashboardScreen({super.key});
@@ -12,32 +11,34 @@ class DashboardScreen extends ConsumerWidget {
     final String value = ref.watch(helloWorldProvider);
 
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
+          backgroundColor: const Color(0xFF121212),
           title: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Image.asset(
-                "assets/appIcon.png",
-                width: 48,
+                "assets/appLogo.png",
+                width: 192,
                 height: 48,
-              ),
-              const SizedBox(width: 8),
-              Text(
-                'BOTANICABAY',
-                style: GoogleFonts.rubik(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
               ),
             ],
           ),
-          backgroundColor: Colors.black87,
         ),
-        body: Center(
-          child: Text(value),
+        backgroundColor: const Color(0xFF121212),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Center(
+              child: Text(
+                value,
+                style: const TextStyle(
+                  color: Color(0xFFFFFFFF),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
